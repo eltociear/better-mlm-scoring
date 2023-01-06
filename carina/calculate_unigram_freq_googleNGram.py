df691cc8f33e84a568f4170e99870c0a318da322
carina-kauf <ckauf@mit.edu>
2023-01-06 09:22:40 -0500

    unigram freqs

--- carina/calculate_unigram_freq_googleNGram.py
+++ carina/calculate_unigram_freq_googleNGram.py
@@ -4,13 +4,13 @@ import numpy as np
 import os
 
 def main():
-    out_dir = 'unigram_frequencies/'
+    out_dir = 'results/unigram_frequencies/'
     os.makedirs(out_dir, exist_ok=True)
 
     with open(os.path.abspath("eventsAdapt/EventsAdapt_vocabulary.txt")) as file:
         words = [line.rstrip() for line in file]
     
-    with open(os.path.abspath("unigram_frequencies/EventsAdapt_unigram_frequencies.txt"), "w") as fout:
+    with open(os.path.abspath("results/unigram_frequencies//EventsAdapt_unigram_frequencies.txt"), "w") as fout:
         
         fout.write('Index\tWord\tUnigram_freq\tUnigram_prob\n') #add header
         
